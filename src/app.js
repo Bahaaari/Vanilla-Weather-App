b51c9b8202ef7c0f362b71a937a55ba6335a9571
Bahaaari <87390415+Bahaaari@users.noreply.github.com>
2021-09-12 20:49:27 +0430

    some change in html and add js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+function weatherCondition(response) {
+  console.log(response);
+  //let temperatureElement = document.querySelector("#temperature");
+
+  // temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
+let apiKey = "0d4847b8ed5adf866001a54ef0a28029";
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=tehran&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(weatherCondition);
