8ca053f8fc4764535d8d6fbc09a68e3edfeb6f2a
Bahaaari <87390415+Bahaaari@users.noreply.github.com>
2021-09-12 22:27:30 +0430

    add weatherCondition & formatDate

--- src/app.js
+++ src/app.js
@@ -1,12 +1,49 @@
-function weatherCondition(response) {
-  console.log(response);
-  //let temperatureElement = document.querySelector("#temperature");
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
 
-  // temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function weatherCondition(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let skyDescriptionElement = document.querySelector("#skyDescription");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#windSpeed");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  skyDescriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+  );
 }
 
 let apiKey = "0d4847b8ed5adf866001a54ef0a28029";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=tehran&appid=${apiKey}&units=metric`;
+let city = "Tehran";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(weatherCondition);
